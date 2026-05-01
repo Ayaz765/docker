9744bb967dad3097f29c9c46c617b4956a7eb48c
ayaz765 <ayazahmad984@gmail.com>
2026-05-01 11:02:44 +0530

    added selenium execution in CI

--- src/main/java/com/tricentis/demowebshop/utils/WebDriverFactory.java
+++ src/main/java/com/tricentis/demowebshop/utils/WebDriverFactory.java
@@ -8,61 +8,80 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.time.Duration;
+import java.net.URL;
 
-/**
- * Creates WebDriver instances with WebDriverManager-managed drivers.
- */
 public final class WebDriverFactory {
 
-    private static final Logger log = LoggerFactory.getLogger(WebDriverFactory.class);
-
-    private WebDriverFactory() {
-    }
+    private WebDriverFactory() {}
 
     public static WebDriver createDriver() {
         String browser = ConfigReader.get("browser", "chrome").toLowerCase();
         boolean headless = Boolean.parseBoolean(ConfigReader.get("browser.headless", "true"));
-        WebDriver driver = switch (browser) {
-            case "firefox" -> {
-                WebDriverManager.firefoxdriver().setup();
-                FirefoxOptions o = new FirefoxOptions();
-                if (headless) {
-                    o.addArguments("-headless");
-                }
-                yield new FirefoxDriver(o);
-            }
-            case "edge" -> {
-                WebDriverManager.edgedriver().setup();
-                EdgeOptions o = new EdgeOptions();
-                if (headless) {
-                    o.addArguments("--headless=new", "--disable-gpu", "--window-size=1920,1080");
-                }
-                yield new EdgeDriver(o);
-            }
-            default -> {
-                WebDriverManager.chromedriver().setup();
-                ChromeOptions o = new ChromeOptions();
-                if (headless) {
-                    o.addArguments("--headless=new", "--disable-gpu", "--no-sandbox", "--window-size=1920,1080");
-                }
-                yield new ChromeDriver(o);
-            }
-        };
+        boolean remote = Boolean.parseBoolean(ConfigReader.get("remote.execution", "false"));
 
-        int implicit = ConfigReader.getInt("implicit.wait.seconds", 0);
-        if (implicit > 0) {
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicit));
+        WebDriver driver;
+
+        try {
+            if (remote) {
+                String gridUrl = ConfigReader.get("grid.url", "http://localhost:4444/wd/hub");
+
+                switch (browser) {
+                    case "firefox" -> {
+                        FirefoxOptions options = new FirefoxOptions();
+                        if (headless) options.addArguments("-headless");
+                        driver = new RemoteWebDriver(new URL(gridUrl), options);
+                    }
+                    case "edge" -> {
+                        EdgeOptions options = new EdgeOptions();
+                        if (headless) {
+                            options.addArguments("--headless=new", "--disable-gpu", "--window-size=1920,1080");
+                        }
+                        driver = new RemoteWebDriver(new URL(gridUrl), options);
+                    }
+                    default -> {
+                        ChromeOptions options = new ChromeOptions();
+                        if (headless) {
+                            options.addArguments("--headless=new", "--disable-gpu", "--no-sandbox", "--window-size=1920,1080");
+                        }
+                        driver = new RemoteWebDriver(new URL(gridUrl), options);
+                    }
+                }
+
+            } else {
+                // 🔥 LOCAL EXECUTION
+                switch (browser) {
+                    case "firefox" -> {
+                        WebDriverManager.firefoxdriver().setup();
+                        FirefoxOptions options = new FirefoxOptions();
+                        if (headless) options.addArguments("-headless");
+                        driver = new FirefoxDriver(options);
+                    }
+                    case "edge" -> {
+                        WebDriverManager.edgedriver().setup();
+                        EdgeOptions options = new EdgeOptions();
+                        if (headless) {
+                            options.addArguments("--headless=new", "--disable-gpu");
+                        }
+                        driver = new EdgeDriver(options);
+                    }
+                    default -> {
+                        WebDriverManager.chromedriver().setup();
+                        ChromeOptions options = new ChromeOptions();
+                        if (headless) {
+                            options.addArguments("--headless=new", "--disable-gpu", "--no-sandbox");
+                        }
+                        driver = new ChromeDriver(options);
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create driver", e);
         }
-        int pageLoadSec = ConfigReader.getInt("page.load.timeout.seconds", 60);
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(pageLoadSec));
+
         driver.manage().window().maximize();
-        log.info("WebDriver started: browser={}, headless={}, implicitWaitSec={}, pageLoadTimeoutSec={}",
-                browser, headless, implicit, pageLoadSec);
-        log.debug("Driver class: {}", driver.getClass().getName());
         return driver;
     }
 }
